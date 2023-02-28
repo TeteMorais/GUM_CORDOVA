a4497263a57fe80df80e1f760df914775709e42d
Israel Emanuel Nascimento Morais <teresamorais500@gmail.com>
2023-02-28 12:13:35 +0100

    Estrutura Refeita para exportar para o telemovel como apk

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -12,12 +12,20 @@ const routes: Routes = [
         loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
       },
       {
+        path: 'buscar',
+        loadChildren: () => import('../buscar/buscar.module').then(m => m.BuscarPageModule)
+      },
+      {
         path: 'tab2',
         loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
       },
       {
         path: 'tab3',
         loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
+      },
+      {
+        path: 'info',
+        loadChildren: () => import('../info/info.module').then(m => m.InfoPageModule)
       },
       {
         path: '',
